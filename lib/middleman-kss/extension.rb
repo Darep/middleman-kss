a8a62f1b966346277d313a376e91894b750bd9f2
Antti-Jussi Kovalainen <ajk@ajk.fi>
2013-11-09 19:43:05 +0200

    Refactor rendering logic in extension.rb

--- lib/middleman-kss/extension.rb
+++ lib/middleman-kss/extension.rb
@@ -35,22 +35,23 @@ module Middleman
     module Helpers
       # Renders a styleblock with or without styleguide information.
       #
-      # @param [String] tile
-      #   Name of the style tile file to render.
+      # @param [String] name
+      #   Name of the styleblock file to render.
       # @param [Hash] options
       #   Options for rendering.
       # @option options [String] :section
       #   KSS section number (e.g. "1.1") for fetching the styleguide information.
       #
       # @return [String] Generated HTML.
-      def styleblock(tile, options = {})
-        @block_html = self.styleblock_html(tile)
+      def styleblock(name, options = {})
         @styleguide = self.parse_styleguide
+        @block_html = self.render_styleblock(name)
 
         if options.has_key?(:section)
-          self.render_rich_styleblock(options)
+          @section = @styleguide.section(options[:section])
+          self.render_rich_styleblock(name, options)
         else
-          self.render_plain_styleblock
+          self.render_plain_styleblock(name)
         end
       end
 
@@ -67,24 +68,6 @@ module Middleman
 
       protected
 
-        # Render a rich styleblock using the styleguide block template
-        def render_rich_styleblock(options)
-          @section = @styleguide.section(options[:section])
-
-          if @section.blank? or @section.description.blank?
-            raise "Section must have a description. Section #{options[:section]} does not have one or section does not exist."
-          end
-
-          # Render the styleguide block
-          template = ::Tilt.new(self.styleguide_block_path)
-          template.render(self)
-        end
-
-        # Render the plain styleblock HTML
-        def render_plain_styleblock
-          @block_html.gsub('$modifier_class', '').gsub(' class=""', '').prepend('<div class="styleguide-styleblock">') << '</div>'
-        end
-
         def parse_styleguide
           # Parse the KSS style guide once per request (because it probably changes every request)
           unless request.has_key?(:styleguide)
@@ -95,18 +78,32 @@ module Middleman
           return request[:styleguide]
         end
 
-        def styleblock_html(tile_name)
-          tile_file = "_#{tile_name}.html.erb"
-          tile_path = File.join(self.source_dir, self.extension_options[:styleblock_path], tile_file)
-          ::Tilt.new(tile_path).render(self)
+        # Render a rich styleblock using the styleguide block template
+        def render_rich_styleblock(name, options)
+          if @section.blank? or @section.description.blank?
+            raise "Section must have a description. Section #{options[:section]} does not have one or section does not exist."
+          end
+
+          # Render the styleguide block
+          ::Tilt.new(self.styleguide_block_path).render(self)
+        end
+
+        # Render the plain styleblock HTML
+        def render_plain_styleblock(name)
+          @block_html.gsub('$modifier_class', '').gsub(' class=""', '').prepend('<div class="styleguide-styleblock">') << '</div>'
+        end
+
+        def render_styleblock(name)
+          ::Tilt.new(styleblock_path(name)).render(self)
+        end
+
+        def styleblock_path(name)
+          File.join(self.source_dir, self.extension_options[:styleblock_path], "_#{name}.html.erb")
         end
 
         def styleguide_block_path
-          if extension_options[:custom_styleguide_block]
-            File.join(self.source_dir, self.extension_options[:styleguide_block_file])
-          else
-            File.join(File.dirname(__FILE__), self.extension_options[:styleguide_block_file])
-          end
+          base_path = extension_options[:custom_styleguide_block] ? self.source_dir : File.dirname(__FILE__)
+          File.join(base_path, self.extension_options[:styleguide_block_file])
         end
 
         def extension_options
